9aadcdd8471e5338ecb639b47b2117daa369c702
Lasse <lassetange@gmail.com>
2018-06-12 13:17:14 +0200

    Remove /public folder on branch

--- public/path---404-66a4e3a7f19e9a5419b0.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xe70826b53c04],{330:function(t,e){t.exports={data:{site:{siteMetadata:{title:"lasse.tech"}}},pathContext:{}}}});
-//# sourceMappingURL=path---404-66a4e3a7f19e9a5419b0.js.map
