01f59d8163e3df72eb9ccc296066cdd469a7348f
Lasse <lassetange@gmail.com>
2018-06-13 15:01:46 +0200

    Updates

--- path---404-66a4e3a7f19e9a5419b0.js
+++ path---404-66a4e3a7f19e9a5419b0.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xe70826b53c04],{331:function(t,e){t.exports={data:{site:{siteMetadata:{title:"lasse.tech"}}},pathContext:{}}}});
+webpackJsonp([0xe70826b53c04],{333:function(t,e){t.exports={data:{site:{siteMetadata:{title:"lasse.tech"}}},pathContext:{}}}});
 //# sourceMappingURL=path---404-66a4e3a7f19e9a5419b0.js.map
--- path---404-html-66a4e3a7f19e9a5419b0.js
+++ path---404-html-66a4e3a7f19e9a5419b0.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xa2868bfb69fc],{330:function(t,a){t.exports={data:{site:{siteMetadata:{title:"lasse.tech"}}},pathContext:{}}}});
+webpackJsonp([0xa2868bfb69fc],{332:function(t,a){t.exports={data:{site:{siteMetadata:{title:"lasse.tech"}}},pathContext:{}}}});
 //# sourceMappingURL=path---404-html-66a4e3a7f19e9a5419b0.js.map
