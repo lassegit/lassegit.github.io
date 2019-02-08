e3e00dcc0c8751e170b05bc22f49c7570d9fd7bb
Lasse <lassetange@gmail.com>
2019-02-08 18:12:21 +0100

    Fixing flawed code

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,6 @@ import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { favicon } from '../assets/img';
-
 import '../assets/scss/style.scss';
 
 const Layout = ({ children, location }) => (
