62d247f8c65712c09b1d33d2ccc65133c4694a9f
Lasse <lassetange@gmail.com>
2019-04-12 14:15:22 +0200

    Text changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,9 +49,9 @@ const IndexPage = ({ data }) => (
       <div>
         <h2>About me</h2>
         <p>
-          Fullstack developer <b>(primarily frontend)</b> with 10 years of experience in planning, programming and deploying
-          a wide range of web apps. I provide the technical and business expertise to bring your projects and ideas to life.
-          I have a good eye for UX/UI, a good problem solver and strive to write high-quality code (simple, structured and
+          Fullstack developer — primarily frontend — with 10 years of experience in planning, programming and deploying a
+          wide range of web apps. I provide the technical and business expertise to bring your projects and ideas to life. I
+          have a good eye for UX/UI, a good problem solver and strive to write high-quality code (simple, structured and
           maintainable). I welcome challenges and the possibility to get to know new technologies and people.
         </p>
         <p>
