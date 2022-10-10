389b1255db3741b2f3d71223a4ba8d2e9c611a66
Lasse <lassetange@gmail.com>
2022-10-10 14:03:59 +0200

    Updates

--- path---404-html-66a4e3a7f19e9a5419b0.js
+++ path---404-html-66a4e3a7f19e9a5419b0.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xa2868bfb69fc],{339:function(t,a){t.exports={data:{site:{siteMetadata:{title:"lasse.tech"}}},pathContext:{}}}});
+webpackJsonp([0xa2868bfb69fc],{338:function(t,a){t.exports={data:{site:{siteMetadata:{title:"lasse.tech"}}},pathContext:{}}}});
 //# sourceMappingURL=path---404-html-66a4e3a7f19e9a5419b0.js.map
