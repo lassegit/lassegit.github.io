59e988b24f7aff6be47734a94ef289dc97a41b8a
Lasse <lassetange@gmail.com>
2026-07-06 19:55:53 +0200

    Update email

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,8 +65,8 @@ const IndexPage = ({ data }) => (
         <ul className="links">
           <li>
             <img alt="" src={mail} />{' '}
-            <a href="mailto:lassetange@gmail.com" title="Send me an email">
-              lassetange@gmail.com
+            <a href="mailto:lasse@lassetange.com" title="Send me an email">
+              lasse@lassetange.com
             </a>
           </li>
           <li>
